9fd2330e0019716d6ddac1db656a84242bf1a793
Ayush_Singal <ayush20365@iiitd.ac.in>
2022-04-10 17:20:10 +0530

    2.0

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT;
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views') )
 app.get('*',(req,res)=>{
     res.render('home.ejs');
 })
-app.listen('healthatlas-c1e3sua2c-optimus2703.vercel.app',()=>{
+app.listen(port || 3000,()=>{
     console.log("connected");
 })
