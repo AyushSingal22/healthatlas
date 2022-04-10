85118d6a3d1bb3f6b90ca46b243429baffe2f036
Ayush_Singal <ayush20365@iiitd.ac.in>
2022-04-10 17:46:19 +0530

    --

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.set('views',path.join(__dirname,'views') )
 app.get('/',(req,res)=>{
     res.render('home.ejs');
 })
+app.get('/about',(req,res)=>{
+    res.send("under construction");
+})
 app.listen(port || 3000,()=>{
     console.log("connected");
 })
