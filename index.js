77ae28ede3c5a557a5cd7d578d056fafc7bb88be
Ayush_Singal <ayush20365@iiitd.ac.in>
2022-04-10 17:44:18 +0530

    -

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = process.env.PORT;
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views') )
-app.get('*',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('home.ejs');
 })
 app.listen(port || 3000,()=>{
